e2f86542606d26828d79897f6489abb1b76acf4c
Rahul Dubey <rahuldubey7398@gmail.com>
2024-06-16 19:55:13 +0530

    behind the scenes logic added

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
 'use strict';
 
-function updateHS() {
+function updateInnerHTML(selectorName, text) {
+  document.querySelector(selectorName).innerHTML = text;
+}
+
+function randomizeGuess() {
+  toBeGuessed = Math.floor(Math.random() * 20) + 1;
+  console.log({ toBeGuessed });
+}
+
+function updateHS(currScore) {
   if (window.localStorage.getItem('guess-high-score') === null) {
-    window.localStorage.setItem('guess-high-score', 0);
+    if (currScore) window.localStorage.setItem('guess-high-score', currScore);
+    else window.localStorage.setItem('guess-high-score', 0);
   }
 
   let score = window.localStorage.getItem('guess-high-score');
-
-  document.querySelector(
-    '.label-highscore'
-  ).innerHTML = `🥇 Highscore: <span class="highscore">${score}</span>`;
+  if (currScore && currScore > score)
+    window.localStorage.setItem('guess-high-score', currScore);
+  score = window.localStorage.getItem('guess-high-score');
+  updateInnerHTML(
+    '.label-highscore',
+    `🥇 Highscore: <span class="highscore">${score}</span>`
+  );
 }
 
+function resetGame() {
+  randomizeGuess();
+  currentScore = 20;
+  updateInnerHTML(
+    '.label-score',
+    `💯 Score: <span class="score">${currentScore}</span>`
+  );
+  document.querySelector('.guess').value = null;
+}
+
+function checkGuess() {
+  const currVal = document.querySelector('.guess').value;
+
+  if (!Boolean(currVal)) return;
+  if (currVal != toBeGuessed) {
+    currentScore -= 1;
+    updateInnerHTML(
+      '.label-score',
+      `💯 Score: <span class="score">${currentScore}</span>`
+    );
+  } else {
+    //winning logic
+    updateHS(currentScore);
+  }
+  document.querySelector('.guess').value = null;
+}
+
+// updateInnerHTML('.message', 'Wrong! Guess Again.');
+let toBeGuessed = 0;
+let currentScore = 20;
+resetGame();
 updateHS();
