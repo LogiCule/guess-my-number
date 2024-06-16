a1e89c69d1db03d86b14b4e7aa909fd8020513ab
Rahul Dubey <rahuldubey7398@gmail.com>
2024-06-16 14:03:17 +0530

    local storage scoring the high score

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function updateHS() {
+  if (window.localStorage.getItem('guess-high-score') === null) {
+    window.localStorage.setItem('guess-high-score', 0);
+  }
+
+  let score = window.localStorage.getItem('guess-high-score');
+
+  document.querySelector(
+    '.label-highscore'
+  ).innerHTML = `🥇 Highscore: <span class="highscore">${score}</span>`;
+}
+
+updateHS();
